c494a8d66ecb88ff862224d72b6d4653b728a305
Lucas <lucaschmain@gmail.com>
2025-03-18 22:37:57 -0300

    test: adding authenticate unit test

---
+++ src/domain/users/application/use-cases/authenticate.spec.ts
@@ -0,0 +1,59 @@
+import { InMemoryUsersRepository } from 'test/repositories/in-memory-user-repository'
+import { AuthenticateUseCase } from './authenticate'
+import { FakeEncrypter } from 'test/cryptography/fake-encrypter'
+import { makeUser } from 'test/factories/make-user'
+import { WrongCredentialsError } from './errors/wrong-credentials-error'
+
+let authenticateUseCase: AuthenticateUseCase
+
+let userRepository: InMemoryUsersRepository
+let fakeEncrypter: FakeEncrypter
+
+describe('Authenticate use case', () => {
+  userRepository = new InMemoryUsersRepository()
+  fakeEncrypter = new FakeEncrypter()
+
+  authenticateUseCase = new AuthenticateUseCase(userRepository, fakeEncrypter)
+
+  beforeAll(async () => {
+    const admin = makeUser({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    userRepository.create(admin)
+  })
+
+  it('should be able to authenticate an user', async () => {
+    const result = await authenticateUseCase.execute({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(result.isRight()).toBe(true)
+
+    expect(result.value).toEqual({
+      accessToken: expect.any(String),
+    })
+  })
+
+  it('should not be able to authenticate an user when the email is incorrect', async () => {
+    const result = await authenticateUseCase.execute({
+      email: 'invalid-email',
+      password: '123456',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(WrongCredentialsError)
+  })
+
+  it('should not be able to authenticate an user when the password is incorrect', async () => {
+    const result = await authenticateUseCase.execute({
+      email: 'johndoe@example.com',
+      password: 'invalid-password',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(WrongCredentialsError)
+  })
+})
