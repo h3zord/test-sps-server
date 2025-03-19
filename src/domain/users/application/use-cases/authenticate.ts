bd26d53042a9060dc0161a5b45fc89e0ba8e475b
Lucas <lucaschmain@gmail.com>
2025-03-18 22:31:07 -0300

    feat: adding authenticate use case

---
+++ src/domain/users/application/use-cases/authenticate.ts
@@ -0,0 +1,49 @@
+import { Either, left, right } from '@/core/either'
+import { WrongCredentialsError } from './errors/wrong-credentials-error'
+import { UsersRepository } from '../repositories/user-repository'
+import { Encrypter } from '../cryptography/encrypter'
+
+interface AuthenticateUseCaseRequest {
+  email: string
+  password: string
+}
+
+type AuthenticateUseCaseResponse = Either<
+  WrongCredentialsError,
+  {
+    accessToken: string
+  }
+>
+
+export class AuthenticateUseCase {
+  constructor(
+    private userRepository: UsersRepository,
+    private encrypter: Encrypter,
+  ) {}
+
+  public async execute({
+    email,
+    password,
+  }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+    const user = await this.userRepository.findByEmail(email)
+
+    if (!user) {
+      return left(new WrongCredentialsError())
+    }
+
+    const isPasswordValid = user.password === password
+
+    if (!isPasswordValid) {
+      return left(new WrongCredentialsError())
+    }
+
+    const accessToken = await this.encrypter.encrypt({
+      sub: user.id.toString(),
+      role: user.type,
+    })
+
+    return right({
+      accessToken,
+    })
+  }
+}
